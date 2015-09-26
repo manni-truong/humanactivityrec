9ce855ae1541e9b55a44ca2c531667d54a0d0de5
Manni Truong <manni.truong@woodmac.com>
2015-09-26 13:26:35 +0100

    first commit of course project

---
+++ run_analysis.R
@@ -0,0 +1,25 @@
+# Manni Truong, WOodMac 2015
+#
+# Part of Getting and Cleaning Data course on coursera.
+# Script was created as assignment. 
+
+
+# set paths to data files
+path_to_test <- "./test/X_test.txt"
+path_to_train <- "./train/X_train.txt"
+
+# find classes of data file
+classes_test <- sapply(read.table(path_to_test, nrows = 100), class)
+classes_train <- sapply(read.table(path_to_train, nrows = 100), class)
+
+
+# reading in data
+x_test <- read.table(path_to_test, colClasses = classes_test)
+x_train <- read.table(path_to_train, colClasses = classes_train)
+
+# merging test and train into one set 
+x_merged_set <- rbind(x_test, x_train)
+
+print(dim(x_test))
+print(dim(x_train))
+print(dim(x_merged_set))
