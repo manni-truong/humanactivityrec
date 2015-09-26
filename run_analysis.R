533d1f0c20832fa42ebd25cb71d2db83c8b38c4e
Manni Truong <manni.truong@woodmac.com>
2015-09-26 21:55:41 +0100

    merged, added labels

--- run_analysis.R
+++ run_analysis.R
@@ -8,35 +8,73 @@ library(stringr)
 
 
 # set paths to data files
-path_to_test <- "./test/X_test.txt"
-path_to_train <- "./train/X_train.txt"
+path_to_test_x <- "./test/X_test.txt"
+path_to_test_y <- "./test/y_test.txt"
+path_to_test_subj <- "test/subject_test.txt"
+
+path_to_train_x <- "./train/X_train.txt"
+path_to_train_y <- "./train/y_train.txt"
+path_to_train_subj <- "train/subject_train.txt"
 
 # find classes of data file
-classes_test <- sapply(read.table(path_to_test, nrows = 100), class)
-classes_train <- sapply(read.table(path_to_train, nrows = 100), class)
+classes_test_x <- sapply(read.table(path_to_test_x, nrows = 100), class)
+classes_test_y <- sapply(read.table(path_to_test_y, nrows = 100), class)
+classes_test_subj <- sapply(read.table(path_to_test_subj, nrows = 100), class)
+
+classes_train_x <- sapply(read.table(path_to_train_x, nrows = 100), class)
+classes_train_y <- sapply(read.table(path_to_train_y, nrows = 100), class)
+classes_train_subj <- sapply(read.table(path_to_train_subj, nrows = 100), class)
 
 
 # reading in data
-x_test <- read.table(path_to_test, colClasses = classes_test)
-x_train <- read.table(path_to_train, colClasses = classes_train)
+x_test <- read.table(path_to_test_x, colClasses = classes_test_x)
+y_test <- read.table(path_to_test_y, colClasses = classes_test_y)
+subj_test <- read.table(path_to_test_subj, colClasses = classes_test_subj) 
+
+x_train <- read.table(path_to_train_x, colClasses = classes_train_x)
+y_train <- read.table(path_to_train_y, colClasses = classes_train_y)
+subj_train <- read.table(path_to_train_subj, colClasses = classes_train_subj)
+
+
+# change col names for y sets
+colnames(y_test)[1] <- "activity_id"
+colnames(subj_test)[1] <- "subject_id"
+
+colnames(y_train)[1] <- "activity_id"
+colnames(subj_train)[1] <- "subject_id"
+
+
+# merge cols
+test <- cbind(subj_test, y_test, x_test)
+train <- cbind(subj_train, y_train, x_train)
 
 # merging test and train into one set 
-x_merged_set <- rbind(x_test, x_train)
+merged_set <- rbind(test, train)
 
-print(dim(x_test))
-print(dim(x_train))
-print(dim(x_merged_set))
+print(dim(test))
+print(dim(train))
+print(dim(merged_set))
 
-# reading column names from features.txt
+# reading column names 
 features <- read.table("features.txt")
+activities <- read.table("activity_labels.txt")
 
 # clean up names
 features$V2 <- gsub("-", "_", features$V2)
 features$V2 <- str_replace_all(features$V2, "([_])|[[:punct:]]", "\\1") # remove punctuation bar "_"
 
+# prepend "V" so we can match later
 features$V1 <- paste0("V", features$V1)
 
 
-# change headers
-names(x_merged_set)[names(x_merged_set)==features$V1] <- features$V2
+# Give headers descriptive names from features.txt
+colnames(merged_set)[colnames(merged_set) %in% features$V1] <- features$V2
 
+# set actitvity names
+merged_set$activity_desc <- activities$V2[match(merged_set$activity_id, activities$V1)]
+
+# only extract mean and std
+#clean_x_merged <- x_merged_set[, grep("mean|std", names(x_merged_set))]
+
+# output data in csv
+write.table(merged_set, "merged.csv", sep = ",", row.names = FALSE)
